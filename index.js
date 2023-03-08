f7f1473639d0d5376ef614993704be6ec5246a3d
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-08 00:17:13 -0500

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee =
+{
+    name : "Binil",
+    streetAddress : "24 Cantwell Cres",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+   const newEmployeeObject= {...employee};
+   newEmployeeObject[key]=value;
+   return newEmployeeObject;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+       employee[key]=value;
+       return employee;
+
+}
+
+function  deleteFromEmployeeByKey(employee, key)
+{
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;    
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key];
+    return employee;
+}
